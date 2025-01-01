dbb61e710eec31a487f75b888cdf5716ba233227
mahendar44 <125560306+mahendar44@users.noreply.github.com>
2025-01-01 20:55:29 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -146,22 +146,3 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "eks_cluster_name" {
-  value = aws_eks_cluster.main.name
-}
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
-}
-
-output "eks_cluster_arn" {
-  value = aws_eks_cluster.main.arn
-}
-
-output "node_group_name" {
-  value = aws_eks_node_group.main.node_group_name
-}
